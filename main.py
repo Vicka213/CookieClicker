81c0885b79733088c293e2ba308e99a9958f59f0
Victoria/Vicky <146642559+Vicka213@users.noreply.github.com>
2023-10-01 16:21:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+gens = 15;
+axiom = "A";
+chr_1, rule_1 = "A","AB";
+chr_2, rule_2 = "B","A";
+
+def apply_rules(axiom):
+    res = ' '
+    for chr in axiom:
+        if chr == chr_1:
+            res += rule_1
+        else:
+            res += rule_2
+    return res
+
+for gen in range(gens):
+    input()
+    print(f'generation {gen}: {len(axiom)}')
+    axiom = apply_rules(axiom)
